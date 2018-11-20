9d7b7ab39d95ce62b539aa200981491739cca778
Verquantumica <veronicaburnham@gmail.com>
2018-11-20 18:58:13 -0500

    fixed newsAPI file

--- src/Components/NewsAPI.js
+++ src/Components/NewsAPI.js
@@ -8,38 +8,11 @@ class NewsAPI extends Component {
       candidate: "Kirsten Gillibrand",
       articles: [],
       sources: [], 
-      state: "NY", 
-      votingComparisons: {libAgree: 0, conAgree: 0}
     };
     this.artToState = this.artToState.bind(this);
     this.changeSource = this.changeSource.bind(this);
   }
 
-  async componentDidMount(){ 
-    let memId; 
-    let response = await axios.get(
-      `/api/memId/${this.state.state}`
-    );
-    let candidateName1 = response.data.results[0].first_name + ' ' + response.data.results[0].last_name; 
-    let candidateName2 = response.data.results[1].first_name + ' ' + response.data.results[1].last_name; 
-    if (candidateName1 === this.state.candidate){ 
-      memId = response.data.results[0].id; 
-    } else if (candidateName2 === this.state.candidate){ 
-      memId = response.data.results[1].id; 
-    }
-
-    //need to check if there is an id returned and if so --> then use id to get record
-
-    //then call 2 requests --> one comparing to most liberal and one to most conservative
-    let liberalCompare = await axios.get(`/api/compareLib/${memId}`); 
-    let conservativeCompare = await axios.get(`/api/compareCon/${memId}`); 
-    let libAgree = Math.floor((liberalCompare.data.results[0].agree_percent - 22.88)/77.12*100)
-    let conAgree = Math.floor((conservativeCompare.data.results[0].agree_percent - 22.88)/77.12*100)
-    console.log('comparisons', liberalCompare.data.results, conservativeCompare.data.results)
-    this.setState({ 
-      votingComparisons: {libAgree, conAgree}
-    })
-  }
 
   changeSource(e) {
     let sources = this.state.sources;
@@ -70,8 +43,6 @@ class NewsAPI extends Component {
           </div>
         ))}
 
-        <p>Votes Liberal: {this.state.votingComparisons.libAgree}%</p>
-        <p>Votes Conservative: {this.state.votingComparisons.conAgree}%</p>
       </div>
     );
   }
