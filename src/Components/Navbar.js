748c4152919879589cba7a56444f3225f9569711
Verquantumica <veronicaburnham@gmail.com>
2018-11-26 17:27:29 -0500

    (feat): added navbar styling and admin link

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; 
+import HOC from './HOC'
+
+const Navbar = (props) => { 
+    return ( 
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-danger text-white">
+                <a className="navbar-brand" href="/">Home<span className="sr-only">(current)</span></a>
+                <a className="navbar-brand" href="/info">Candidate Info</a>
+                <a className="navbar-brand" href="/results">Results</a>
+                {props.adminStatus ? (<a className="navbar-brand" href="/admin">Admin</a>) : null}
+                
+            </nav>
+        </div>
+    )
+}
+
+export default HOC(Navbar)
