9733d54901b77ab1bfd43837eca1e58e351da04a
Ay-Nur Najm <aynajm@gmail.com>
2018-11-27 16:28:09 -0500

    font change

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -16,42 +16,22 @@ const Main = props => {
     //   ))}
     // </div>
 
-    // <header class="masthead text-center text-white d-flex">
-    //   <div class="container my-auto">
-    //     <div class="row">
-    //       <div class="col-lg-10 mx-auto">
-    //         <h1 class="bg-light text-primary">
-    //           <strong>Blockchain Voting dApp</strong>
-    //         </h1>
-    //         <hr />
-    //       <div class="col-lg-8 mx-auto">
-    //         <h3 class="mb-5 bg-dark text-white">
-    //           Make Democracy/Voter Turnout Great Again...Coming soon to a
-    //           computer near you!
-    //         </h3>
-    //         <a class="btn btn-primary btn-xl js-scroll-trigger" href="#tech">
-    //           I'm interested!
-    //         </a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </header>
-
-    <header class="masthead text-center text-white d-flex">
-      <div class="container my-auto">
-        <div class="row">
-          <div class="col-lg-10 mx-auto">
-            <h1 class="bg-light text-primary">
-              <strong>Blockchain Voting dApp</strong>
-            </h1>
+    <header className="masthead text-center text-white d-flex">
+      <div className="container my-auto">
+        <div className="row">
+          <div className="col-lg-10 mx-auto">
+            <h1 className="bg-light text-primary">Blockchain Voting dApp</h1>
             <hr />
           </div>
-          <div class="col-lg-8 mx-auto">
-            <h3 class="mb-5 bg-dark text-white">
+          <div className="col-lg-8 mx-auto">
+            <h3 className="mb-5 bg-dark text-white">
               Make Democracy/Voter Turnout Great Again...Coming soon to a
               computer near you!
             </h3>
-            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#tech">
+            <a
+              className="btn btn-primary btn-xl js-scroll-trigger"
+              href="#tech"
+            >
               I'm interested!
             </a>
           </div>
