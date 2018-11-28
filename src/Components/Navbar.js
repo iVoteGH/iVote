56d0313a3a3c32fc911b97484567296e0d772fb3
kaitlinmaier <kaitlin.e.maier@gmail.com>
2018-11-28 12:46:15 -0500

    feat(navbar)

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import HOC from "./HOC";
+import React from 'react';
+import HOC from './HOC';
 
 const Navbar = props => {
   return (
@@ -8,17 +8,23 @@ const Navbar = props => {
         className="navbar navbar-expand-lg navbar-dark bg-danger text-white"
         id="navbar"
       >
-        <a className="navbar-brand" href="/">
-          Home<span className="sr-only">(current)</span>
-        </a>
-        <a className="navbar-brand" href="/info">
-          Candidates
-        </a>
-        <a className="navbar-brand" href="/results">
-          Results
-        </a>
+        <li className="nav-item active">
+          <a className="navbar-brand active" href="/">
+            Home<span className="sr-only">(current)</span>
+          </a>
+        </li>
+        <li className="nav-item active">
+          <a className="navbar-brand active" href="/info">
+            Candidates
+          </a>
+        </li>
+        <li className="nav-item active">
+          <a className="navbar-brand active" href="/results">
+            Results
+          </a>
+        </li>
         {props.adminStatus ? (
-          <a className="navbar-brand" href="/admin">
+          <a className="navbar-brand active" href="/admin">
             Admin
           </a>
         ) : null}
